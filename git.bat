c09adf89afb3a8222421ab3a83462c68ee4c513c
opoliticsguru <opoliticsguru@gmail.com>
2024-11-05 05:09:56 -0500

    autoCommit 2024 1Tu. 50956

---
+++ git.bat
@@ -0,0 +1,5 @@
+cd C:\Users\andre\Downloads\2024_Nov Election Night Mapping\Live Turnout\Live Map
+"C:\Program Files\Git\bin\git.exe" add --all
+"C:\Program Files\Git\bin\git.exe" commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+"C:\Program Files\Git\bin\git.exe" push
+exit
